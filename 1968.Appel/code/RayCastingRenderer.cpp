58c68985c57c16bdf7bcfff104feb4f5440a4aa3
neil3d <neil3d@126.com>
2020-05-22 20:21:04 +0800

    reset ray casting code

--- 1968.Appel/code/RayCastingRenderer.cpp
+++ 1968.Appel/code/RayCastingRenderer.cpp
@@ -41,27 +41,18 @@ void RayCastingRenderer::_drawSinglePixel(int x, int y, MyScene* pScene) {
 
   if (!bHit) return;
 
+  glm::vec4 color;
   Ray shadowRay = _generateShadowRay(hitRec.p);
   HitRecord hitRecS;
   constexpr float SHADOW_E = 0.1f;
   bool bShadow = pScene->hit(shadowRay, SHADOW_E, fMax, hitRecS);
-
-  // according to the paper:
-  // H = size of plus sign
-  // Hs = maximum size of the plus sign
-  // h = a contrast factor, usually .8
-  const float Hs = 1.0f;
-  const float h = 0.8f;
-  float Hj;
-
   if (bShadow) {
-    Hj = Hs;
+    color = glm::vec4(0, 0, 0, 1);
   } else {
     glm::vec3 L = glm::normalize(mLightPos - hitRec.p);
     float c = std::max(0.0f, glm::dot(hitRec.normal, L));
-    Hj = h * (1 - c);
+    color = glm::vec4(c, c, c, 1);
   }
 
-  glm::vec4 color(1 - Hj, 1 - Hj, 1 - Hj, 1.0f);
   _writePixel(x, y, color);
 }
