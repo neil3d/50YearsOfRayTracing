79ab280688672feb6c186920d264182e2b12bd80
neil3d <neil3d@126.com>
2020-06-14 13:58:51 +0800

    create mori knob scene

---
+++ 1986.Kajiya/code/MoriKnobScene.h
@@ -0,0 +1,93 @@
+/**
+ * A Brief History of Ray tracing
+ *
+ * GitHub: https://github.com/neil3d/RayTracingHistory
+ *
+ * Created by yanliang.fyl, 2020
+ */
+
+#pragma once
+#include <memory>
+
+#include "DiffuseMaterial.h"
+#include "MySceneWithLight.h"
+#include "ParallelogramLight.h"
+#include "asset/MaterialImporter.h"
+#include "scene/MeshInstance.h"
+#include "scene/Parallelogram.h"
+#include "scene/Sphere.h"
+
+namespace RayTracingHistory {
+
+class MoriKnobMtlImporter : public MaterialImporter {
+  virtual MyMaterial::Ptr importObj(const std::string& szName) override {
+    if (szName == "BackGroundMat") {
+      auto mtl = std::make_shared<DiffuseMaterial>();
+      mtl->setColor(glm::vec3(0.25f));
+      return mtl;
+    }
+
+    if (szName == "InnerMat") {
+      auto mtl = std::make_shared<DiffuseMaterial>();
+      mtl->setColor(glm::vec3(0.5f));
+      return mtl;
+    }
+
+    if (szName == "LTELogo") {
+      auto mtl = std::make_shared<DiffuseMaterial>();
+      mtl->setColor(glm::vec3(1, 1, 0));
+      return mtl;
+    }
+
+    if (szName == "OuterMat") {
+      auto mtl = std::make_shared<DiffuseMaterial>();
+      mtl->setColor(glm::vec3(0, 1, 1));
+      return mtl;
+    }
+
+    return std::make_shared<DiffuseMaterial>();
+  }
+};
+
+class MoriKnobScene : public MySceneWithLight {
+  ParallelogramLight mMainLight;
+
+  void _initLight() {
+    glm::vec3 v1(3.75, 12, -3.75);
+    glm::vec3 v2(-3.75, 12, 3.75);
+    glm::vec3 v3(-3.75, 12, -3.75);
+    glm::vec3 v4(3.75, 12, 3.75);
+
+    glm::vec3 edge1 = v3 - v1;
+    glm::vec3 edge2 = v4 - v1;
+    mMainLight.setShape(edge1, edge2, v1);
+
+#if 0
+    createObject<Parallelogram>("light_shape")
+        .setEdges(edge1, edge2)
+        .setAnchor(v1)
+        .createMaterial<DiffuseMaterial>()
+        .setColor(glm::vec3(1))
+        .enableLight();
+#endif
+  }
+
+ public:
+  virtual const AreaLight* getMainLight() const override { return &mMainLight; }
+
+  virtual float systemUnit() const { return 1; }
+
+  virtual void init() override {
+    const char* const szFileName = "content/mori_knob/testObj.obj";
+
+    auto& mesh = createObject<MeshInstance>("mori_knob");
+    mesh.setMeshFile(szFileName);
+    mesh.setScale(glm::vec3(-1, 1, 1));
+
+    MoriKnobMtlImporter mtlImporter;
+    mesh.importMaterial(&mtlImporter);
+
+    _initLight();
+  }
+};
+}  // namespace RayTracingHistory
--- 1986.Kajiya/code/main.cpp
+++ 1986.Kajiya/code/main.cpp
@@ -3,6 +3,7 @@
 #include "CornellBoxScene.h"
 #include "DemoScene.h"
 #include "MonteCarloPathTracer.h"
+#include "MoriKnobScene.h"
 #include "framework/MyApp.h"
 #include "framework/PinholeCamera.h"
 
@@ -14,6 +15,8 @@ const char* const APP_NAME = "Monte Carlo Path Tracing";
 const uint32_t WINDOW_WIDTH = 1280;
 const uint32_t WINDOW_HEIGHT = 720;
 
+enum class EMyTestScenes { Demo, CornellBox, MoriKnob };
+
 int main(int argc, char* argv[]) {
   MyApp app;
   try {
@@ -21,19 +24,30 @@ int main(int argc, char* argv[]) {
     app.createWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APP_NAME);
     auto renderer = app.createRenderer<MonteCarloPathTracer>();
 
-#if 1
-    auto scene = std::make_shared<CornellBoxScene>();
-    scene->init();
+    EMyTestScenes sceneType = EMyTestScenes::MoriKnob;
+    MyScene::Ptr scene;
+    glm::vec3 eyePos;
+    glm::vec3 lookAt;
 
-    glm::vec3 eyePos(278, 278, -656);
-    glm::vec3 lookAt(278, 278, 0);
-#else
-    auto scene = std::make_shared<DemoScene>();
+    switch (sceneType) {
+      case EMyTestScenes::CornellBox:
+        scene = std::make_shared<CornellBoxScene>();
+        eyePos = glm::vec3(278, 278, -656);
+        lookAt = glm::vec3(278, 278, 0);
+        break;
+      case EMyTestScenes::MoriKnob:
+        scene = std::make_shared<MoriKnobScene>();
+        eyePos = glm::vec3(0, 1, -2);
+        lookAt = glm::vec3(0, 0, 0);
+        break;
+      case EMyTestScenes::Demo:
+      default:
+        scene = std::make_shared<DemoScene>();
+        eyePos = glm::vec3(0, 250, -850);
+        lookAt = glm::vec3(0, 250, 0);
+        break;
+    }  // end of scene
     scene->init();
-
-    glm::vec3 eyePos(0, 250, -850);
-    glm::vec3 lookAt(0, 250, 0);
-#endif
 
     auto camera = std::make_shared<PinholeCamera>();
     camera->setZNear(0.1f)
